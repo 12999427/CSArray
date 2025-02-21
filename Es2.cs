7eb3413cce2fce4ab58564292140d0816bb516ef
12999427 <12999427@itisrossi.vi.it>
2025-02-21 10:05:41 +0100

    Add files via upload

---
+++ Es2.cs
@@ -0,0 +1,28 @@
+﻿namespace es
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random(Environment.TickCount);
+            int[] a = new int[30];
+            for (int i = 0; i<30; i++)
+            {
+                a[i] = random.Next(0, 101);
+                Console.Write(a[i] + "\t");
+                
+            }
+            Console.WriteLine();
+            for (int i = 0; i<30; i++)
+            {
+                for (int j = 0; j<30; j += 2)
+                {
+                    if (a[i]*2 == a[j])
+                    {
+                        Console.WriteLine($"Coppia di numeri: {a[i]} e {a[j]}");
+                    }
+                }
+            }
+        }
+    }
+}
