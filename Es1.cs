8f7be254f02c9e3733bb2eeee7c2a6e4805f2920
12999427 <12999427@itisrossi.vi.it>
2025-02-21 10:04:59 +0100

    Add files via upload

---
+++ Es1.cs
@@ -0,0 +1,38 @@
+﻿using static System.Console;
+
+namespace es
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lung = 0;
+            do
+            {
+                WriteLine("dimmi la lunghezza dell'array");
+            } while (!Int32.TryParse(ReadLine(), out lung) || lung <= 0);
+            int[] numeri = new int[lung];
+
+            for (int i = 0; i < numeri.Length; i++)
+            {
+                do
+                {
+                    WriteLine("dimmi il numero");
+                } while (!Int32.TryParse(ReadLine(), out numeri[i]));
+            }
+
+            for (int j = 0; j < numeri.Length; j++)
+            {
+                if (j != numeri.Length-1)
+                {
+                    if (numeri[j] == numeri[j+1])
+                    {
+                        WriteLine("0");
+                        return;
+                    }
+                }
+            }
+            WriteLine("1");
+        }
+    }
+}
